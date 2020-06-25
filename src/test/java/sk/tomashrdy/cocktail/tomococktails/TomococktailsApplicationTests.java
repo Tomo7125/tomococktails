6a85977f9dbbd169a8005c855b7ef31ceb8331fc
Tomo <tomashrdy3@gmail.com>
2020-06-25 20:59:18 +0200

    Spring initializr

---
+++ src/test/java/sk/tomashrdy/cocktail/tomococktails/TomococktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.tomashrdy.cocktail.tomococktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomococktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
