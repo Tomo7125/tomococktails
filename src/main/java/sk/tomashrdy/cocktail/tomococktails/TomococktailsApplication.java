b508256ba697490ad904e60db1bf38c01e3841a1
Tomo <tomashrdy3@gmail.com>
2020-06-25 20:56:14 +0200

    Spring initializr

---
+++ src/main/java/sk/tomashrdy/cocktail/tomococktails/TomococktailsApplication.java
@@ -0,0 +1,13 @@
+package sk.tomashrdy.cocktail.tomococktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomococktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomococktailsApplication.class, args);
+	}
+
+}
